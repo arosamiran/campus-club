04da3fc74fe79233944d0d87f3ef6960d4494208
arosamiran <arosamiran@outlook.com>
2017-11-14 19:44:47 +0800

    removed some examples

--- interactivity.js
+++
@@ -1,9 +0,0 @@
-let sphere = document.querySelector("#sphere");
-sphere.addEventListener("mouseenter", changeColor());
-
-function changeColor() {
-  sphere.setAttribute("color", "#0f0");
-  setTimeout(function() {
-    sphere.setAttribute("color", "#00f")
-  }, 2000);
-}
